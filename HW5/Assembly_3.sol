ad643d5032708f51a3812f536b0a7cb318dbde4d
Vadzim Mahilny <magilnyy.vadimo@gmail.com>
2023-08-01 22:39:10 +0100

    Assembly

---
+++ HW5/Assembly_3.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract SubOverflow {
+    // Modify this function so that on overflow it returns the value 0
+    // otherwise it should return x - y
+    function subtract(uint256 x, uint256 y) public pure returns (uint256) {
+        // Write assembly code that handles overflows
+        assembly {
+            let result := sub(x, y)
+            if lt(result, 0) { mstore(0x0, 0) }
+            if gt(result, 0) { mstore(0x0, result)}
+            return(0x0, 32)
+        }
+    }
+}
